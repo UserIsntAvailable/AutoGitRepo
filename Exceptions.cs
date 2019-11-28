6deb436e92c4a77d713205f30151c9cc279efe6f
Angel Pineda <hometoys1611@gmail.com>
2019-11-28 00:05:54 -0500

    Exceptions.cs added and Program.cs have a better argument handle

---
+++ Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace AutoGitRepo {
+
+    class ArgumentNameNotUsedException : Exception {
+
+        public ArgumentNameNotUsedException() {
+        }
+
+        public ArgumentNameNotUsedException(string message) : base(message) {
+        }
+    }
+
+    class ArgumentNotImplementedException : Exception {
+
+        public ArgumentNotImplementedException() {
+        }
+
+        public ArgumentNotImplementedException(string message) : base(message) {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,99 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace AutoGitRepo {
     class Program {
 
-        private static int lineCount = 0;
-        private static readonly StringBuilder output = new StringBuilder();
+        #region Private Properties
+        private static int LineCount { get; set; }
+        private static StringBuilder Output { get; set; } = new StringBuilder();
+
+        private static string RepositoryName { get; set; }
+        private static string RepositoryDescription { get; set; } = "";
+        private static string GitIgnore { get; set; } = "";
+        private static bool IsPrivate { get; set; } = false;
+        #endregion
 
         static void Main(string[] args) {
 
+            // If 0 arguments are given print description of the app and help argument 
+            if (args.Length == 0) {
+                Console.WriteLine("This is a tool made for automate the process of create a github repository.\n\nA help argument will be added soon :)");
+                return;
+            }
+
+            // Else run the command checker
+            else {
+                // Loop each argument in args
+                for (int i = 0; i < args.Length; i += 2) {
+                    switch (args[0 + i]) {
+
+                        case "--name":
+                        case "-n":
+                            if (args.Contains("--name") & args.Contains("-n")) {
+                                throw new RepetedArgumentsException("--name and -n are repeted");
+                            }
+                            RepositoryName = args[1 + i];
+                            break;
+
+                        case "--description":
+                        case "-d":
+                            if (args.Contains("--description") & args.Contains("-d")) {
+                                throw new RepetedArgumentsException("--description and -d are repeted");
+                            }
+                            RepositoryDescription = args[1 + i];
+                            break;
+
+                        case "--is_private":
+                        case "-p":
+                            if (args.Contains("--is_private") & args.Contains("-p")) {
+                                throw new RepetedArgumentsException("--is_private and -p are repeted");
+                            }
+                            IsPrivate = true;
+                            // This argument dosen't need a parameter.
+                            i -= 1;
+                            break;
+
+                        case "--gitignore":
+                        case "-g":
+                            if (args.Contains("--gitignore") & args.Contains("-g")) {
+                                throw new RepetedArgumentsException("--gitignore and -g are repeted");
+                            }
+                            GitIgnore = args[1 + i];
+                            break;
+
+                        case "--help":
+                        case "-h":
+                            if (args.Contains("--help") & args.Contains("-h")) {
+                                throw new RepetedArgumentsException("--help and -h are repeted");
+                            }
+                            Console.WriteLine("This will added soon");
+                            return;
+
+                        default:
+                            throw new ArgumentNotImplementedException($"The argument {args[0 + i]} is not implement yet");
+                    }
+                }
+
+                // Check if the Repository Name it wasn't initialized
+                if(RepositoryName == null) {
+                    throw new ArgumentNameNotUsedException("--name(-n) is a obligatory argument");
+                }
+            }
+
             // Initializer of json
-            List<Parameters> jsonData = new List<Parameters> {
-                new Parameters() {
-                    name = args[0],
-                    description = args[1],
-                    @private = args[2].Contains("true"),
+            List<JsonAttributes> jsonData = new List<JsonAttributes> {
+                new JsonAttributes() {
+                    name = RepositoryName,
+                    description = RepositoryDescription,
+                    @private = IsPrivate,
                     auto_init = true,
-                    gitignore_template = args[3],
+                    gitignore_template = GitIgnore,
                     license_template = "mit"
                 }
             };
@@ -65,10 +139,12 @@ echo $page_url";
 
                 // Event that catch the output from run.bat
                 mainProcess.OutputDataReceived += new DataReceivedEventHandler((sender, e) => {
-                    // Prepend line numbers to each line of the output.
+
+
+                    // Append line numbers to each line of the output.
                     if (!string.IsNullOrEmpty(e.Data)) {
-                        lineCount++;
-                        output.Append("\n[" + lineCount + "]: " + e.Data);
+                        LineCount++;
+                        Output.Append("\n[" + LineCount + "]: " + e.Data);
                     }
                 });
 
@@ -83,7 +159,7 @@ echo $page_url";
                 using (Process gitProcess = new Process()) {
 
                     // The link for the repo that was created
-                    string page_url = output.ToString().Replace("\n", "").Replace("[1]: ", "");
+                    string page_url = Output.ToString().Replace("\n", "").Replace("[1]: ", "");
 
                     // Process Info
                     gitProcess.StartInfo = new ProcessStartInfo {
@@ -114,8 +190,7 @@ echo $page_url";
             }
         }
     }
-    // Attributes for the json file
-    public class Parameters {
+    class JsonAttributes {
         public string name { get; set; }
         public string description { get; set; }
         public bool @private { get; set; }
